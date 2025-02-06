dcc8f19747bdf9ec8b21348617d186ae3eba2d60
mehbub17 <mehbubhussain.17@gmail.com>
2025-02-06 23:11:34 +0530

    MongoDB connection set

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import process from 'process';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
+}
+connectDB();
 
 const app  = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);   
 });
+
