72068ae747089266cdca851e0f59a82f940cb70d
mehbub17 <mehbubhussain.17@gmail.com>
2025-02-07 17:40:38 +0530

    Signup page completed

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import process from 'process';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cors from 'cors';
 
 
 dotenv.config();
@@ -15,14 +16,15 @@ const connectDB = async () => {
         console.log(`Error: ${error.message}`);
     }
 }
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 const app  = express();
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);   
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);   
 });
 
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/user', userRouter);
